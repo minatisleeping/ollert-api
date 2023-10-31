b7e5e7691d566b3b7fb0692b857ea6acc82795fc
minatisleeping <minatt2002@gmail.com>
2023-10-31 17:38:57 +0700

    done about array

---
+++ 7. Array/index.js
@@ -0,0 +1,15 @@
+
+
+// Viết hàm tại đây
+function getFirstElement(values) {
+    return values[0];
+}
+
+
+
+// Ví dụ sử dụng
+var animals = ['Monkey', 'Tiger', 'Elephant'];
+var result = getFirstElement(animals);
+
+console.log(result); // Expected: "Monkey"
+console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
