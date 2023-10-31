1693a05a17e444b2e58d34a42c385c1fe4541ab2
minatisleeping <minatt2002@gmail.com>
2023-10-31 17:35:48 +0700

    done p2

---
+++ 2. Variables, cmts, built-in/index.js
@@ -0,0 +1,32 @@
+
+
+var namee = 'minat'
+
+console.log('This purpose of line is announcement!'); 
+console.log(name);
+
+/**
+ * Kiểu dữ liệu trong Javascript
+    1. Dữ liệu nguyên thuỷ - Primitive Data
+    - Number
+    - String
+    - Boolean
+    - Undefined
+    - Null
+    - Symbol
+
+    2. Dữ liệu phức tạp - Complex Data
+    - Function
+    - Object
+ */
+
+    // Number type 
+    /**
+     *  var a = 1
+        var b = 2 
+        var c = 3.5
+     */
+
+
+    // String type 
+     
