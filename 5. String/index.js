b5b2830cc402cbc3953170aa64f9f0f6ff2ad1c2
minatisleeping <minatt2002@gmail.com>
2023-10-31 17:38:11 +0700

    done about string

---
+++ 5. String/index.js
@@ -0,0 +1,8 @@
+function getUpperCaseName(name) {
+    return name.toUpperCase();
+}
+
+
+// Expected results:
+console.log(getUpperCaseName("Nguyen van a")) // "NGUYEN VAN A"
+console.log(getUpperCaseName("nGuyen vAn C")) // "NGUYEN VAN C"
