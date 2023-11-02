4f1e4d2409b13a658c7fe157cbc4fc0f6253aef0
minatisleeping <minatt2002@gmail.com>
2023-11-02 11:21:38 +0700

    done about ArrayII

--- 11. Array II/index.js
+++ 11. Array II/index.js
@@ -79,3 +79,54 @@ var course = courses.filter(function(course) {
 console.log(course)    // result: 
                                     // 0: {id: 2, name: 'HTML, CSS', cost: 0}
                                     // 1: {id: 6, name: 'HTML, CSS', cost: 123}
+
+
+//reduce() 
+var totalCost = courses.reduce(function(accumulate, currentValue) {
+    return accumulate + currentValue.cost;
+}, 0)
+console.log(totalCost) // result: 1273
+
+// Exercise about reduce() - Tại SEA GAMES 31 vừa qua, đoàn thể thao Việt Nam 
+//đã đứng đầu bảng tổng sắp huy chương. Hãy tạo hàm getTotalGold có 1 tham số là mảng. 
+//Tính tổng số huy chương vàng mà đoàn thể thao Việt Nam đạt được trong kỳ SEA Game lần này.
+var sports = [
+    {
+        name: 'Bơi lội',
+        gold: 11
+    },
+    {
+        name: 'Boxing',
+        gold: 3
+    },
+    {
+        name: 'Đạp xe',
+        gold: 4
+    },
+    {
+        name: 'Đấu kiếm',
+        gold: 5
+    },
+]
+
+function getTotalGold(arr) {
+    return arr.reduce(function(sum, curValue){
+        return sum + curValue.gold
+    }, 0)
+}
+
+// Expected results:
+console.log(getTotalGold(sports)) // Output: 23
+
+// include() - check string element in arr
+
+var needToLearn = ['SpringBoot', 'NodeJS', 'ReactJS']
+
+console.log(needToLearn.includes('SpringBoot', 0))  // result: true
+console.log(needToLearn.includes('Docker'))      // result: false
+
+
+
+
+
+
