f026ed239dda2ab779cb21113ef751c498ebcda6
minatisleeping <minatt2002@gmail.com>
2024-04-09 06:06:47 +0700

    init code base project

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
@@ -6,9 +7,21 @@ const hostname = 'localhost'
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(3000, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}`)
+app.listen(PORT, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running at http://${hostname}:${PORT}`)
 })
