5c42a6a414f8e2cf44d425f105b2d30169448f79
minatisleeping <minatt2002@gmail.com>
2025-05-16 14:26:31 +0700

    fix: edit whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 //* Vd blacklist là những trang web không được phép truy cập tới tài nguyên Server thì whitelist ngược lại
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-hazel.vercel.app'
 ]
 
 export const BOARD_TYPES = {
