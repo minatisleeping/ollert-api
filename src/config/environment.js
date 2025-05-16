21aa094d6cb56de3656ab5110422fdc649b8c05c
minatisleeping <minatt2002@gmail.com>
2025-05-16 13:29:00 +0700

    feat: deplouy

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,8 +3,8 @@ import 'dotenv/config'
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  HOST: process.env.APP_HOST,
-  PORT: process.env.APP_PORT,
+  HOST: process.env.LOCAL_DEV_APP_HOST,
+  PORT: process.env.LOCAL_DEV_APP_PORT,
   BUILD_MODE: process.env.BUILD_MODE,
   AUTHOR: process.env.AUTHOR
 }
